76f60a51fd8c4e2f1d32886769e5aad8143d8fe1
N.M. Asif Sheikh <nmasifsheikh2003@gmail.com>
2025-06-15 10:57:41 +0600

    Dynamic Banner created

---
+++ src/app/(pages)/events/buy-ticket/page.tsx
@@ -0,0 +1,10 @@
+import BannerDynamicBG from "@/components/Layout/Banners/BannerDynamicBG";
+import React from "react";
+
+export default function BuyTicket() {
+  return (
+    <div>
+      <BannerDynamicBG title="" description="" bgImage="" />
+    </div>
+  );
+}
---
+++ src/components/Layout/Banners/BannerDynamicBG.tsx
@@ -0,0 +1,26 @@
+// import React from "react";
+
+// interface BannerDynamicBGProps {
+//   title: string;
+//   description: string;
+//   bgImage: string;
+// }
+
+// export default function BannerDynamicBG({
+//   title,
+//   description,
+//   bgImage,
+// }: BannerDynamicBGProps) {
+//   return (
+//     <section
+//       className={`relative w-full h-[380px]  text-white overflow-hidden flex flex-col gap-2 justify-center items-center`} // ${ bgImage ? `bg-[url('${bgImage}')]` : "bg-[#1B263B]" }
+//     >
+//       <h1 className="text-[30px] sm:text-[36px] md:text-[40px] font-bold text-center leading-tight max-w-[700px] z-10 px-4">
+//         {title || "Welcome to Ensinor"}
+//       </h1>
+//       <p className="text-base font-normal">
+//         {description || "Description place..."}
+//       </p>
+//     </section>
+//   );
+// }
